90e9f6b7bc5cad9df6a3cefd158596107271a49c
robonexx <robertwagar@gmail.com>
2022-12-08 22:29:13 +0100

    Adding a slider / corousel to frontpage

---
+++ main.js
@@ -0,0 +1,42 @@
+/* const leftBtn = document.querySelector('#left');
+const rightBtn = document.querySelector('#right'); */
+const images = document.querySelector('#imgs');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  images.style.transform = `translateX(${-idx * 800}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 4000);
+}
+
+/* uncomment below to add buttons */
+/* 
+leftBtn.addEventListener('click', () => {
+  idx--;
+  changeImage();
+  resetInterval();
+});
+rightBtn.addEventListener('click', () => {
+  idx++;
+  changeImage();
+  resetInterval();
+}); */
